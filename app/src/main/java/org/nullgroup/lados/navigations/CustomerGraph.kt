6b904290a2a7069bda2d96c65ed91bac623bbd4d
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-11-28 22:53:20 +0700

    Command the ProfileScreen in CustomerGraph

--- app/src/main/java/org/nullgroup/lados/navigations/CustomerGraph.kt
+++ app/src/main/java/org/nullgroup/lados/navigations/CustomerGraph.kt
@@ -1,6 +1,5 @@
 package org.nullgroup.lados.navigations
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material3.Icon
@@ -9,7 +8,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -18,7 +16,6 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import org.nullgroup.lados.screens.Screen
 import org.nullgroup.lados.screens.customer.HomeScreen
-import org.nullgroup.lados.screens.customer.ProfileScreen
 
 @Composable
 fun CustomerGraph(
@@ -65,12 +62,12 @@ fun CustomerGraph(
                         }
 
                         Screen.Customer.Profile.route -> {
-                            ProfileScreen(
-                                modifier = Modifier.padding(
-                                    vertical = 32.dp,
-                                    horizontal = 16.dp
-                                ), paddingValues = innerPadding
-                            )
+//                            ProfileScreen(
+//                                modifier = Modifier.padding(
+//                                    vertical = 32.dp,
+//                                    horizontal = 16.dp
+//                                ), paddingValues = innerPadding
+//                            )
                         }
 
                         Screen.Customer.Order.route -> {
