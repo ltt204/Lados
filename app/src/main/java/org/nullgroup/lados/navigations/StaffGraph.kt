3acee6d847a1c8759f3a0435e044fc2c3e01b62a
Ly'TTin <22120371@student.hcmus.edu.vn>
2024-12-29 14:59:00 +0700

    Add navigation drawer to StaffGraph and AdminGraph; Update screen management

--- app/src/main/java/org/nullgroup/lados/navigations/StaffGraph.kt
+++ app/src/main/java/org/nullgroup/lados/navigations/StaffGraph.kt
@@ -1,34 +1,158 @@
 package org.nullgroup.lados.navigations
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.nullgroup.lados.screens.Screen
+import org.nullgroup.lados.ui.theme.LadosTheme
+import org.nullgroup.lados.viewmodels.customer.profile.ProfileViewModel
 
+private const val TAG = "StaffGraph"
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun StaffGraph(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
-    startDestination: String = Screen.Staff.Reports.route
+    globalNavHostController: NavHostController,
+    startDestination: Screen = Screen.Staff.ChatScreen,
+    drawerState: DrawerState = rememberDrawerState(DrawerValue.Closed),
+    scrollBehavior: TopAppBarScrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(),
+    scope: CoroutineScope = rememberCoroutineScope(),
+    viewModel: ProfileViewModel = hiltViewModel()
 ) {
-    NavHost(navController = navController, startDestination = startDestination) {
-        Screen.Staff.getAllScreens().forEach { screen ->
-            composable(screen.route) {
-                when (screen.route) {
-                    Screen.Staff.ChatScreen.route -> {
-                        // ChatScreen()
-                    }
+    var currentDestination by remember {
+        mutableStateOf(startDestination)
+    }
 
-                    Screen.Staff.Reports.route -> {
-                        // Reports()
-                    }
+    ModalNavigationDrawer(
+        modifier = modifier,
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+                Column {
+                    Text(
+                        "Staff panel",
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                    HorizontalDivider()
+                    NavigationDrawerItem(
+                        label = { Text(text = Screen.Staff.ChatScreen.name!!) },
+                        selected = currentDestination.route == Screen.Staff.ChatScreen.route,
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                            currentDestination = Screen.Staff.ChatScreen
+                            navController.navigate(Screen.Staff.ChatScreen.route)
+                        }
+                    )
+                    NavigationDrawerItem(
+                        label = { Text(text = Screen.Staff.OrderManagement.name!!) },
+                        selected = currentDestination.route == Screen.Staff.OrderManagement.route,
+                        onClick = {
+                            scope.launch {
+                                drawerState.close()
+                            }
+                            currentDestination = Screen.Staff.OrderManagement
+                            navController.navigate(Screen.Staff.OrderManagement.route)
+                        }
+                    )
+                }
+                TextButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { viewModel.signOut(globalNavHostController) }) {
+                    Text(
+                        textAlign = TextAlign.Center,
+                        text = "Sign out",
+                        color = Color.Red,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
+                }
+            }
+        }) {
+        Scaffold(
+            containerColor = LadosTheme.colorScheme.background,
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                        containerColor = LadosTheme.colorScheme.background
+                    ),
+                    title = { Text(text = currentDestination.name!!) },
+                    navigationIcon = {
+                        IconButton(onClick = {
+                            Log.d("AdminTopAppBar", "onDrawerClick")
+                            scope.launch {
+                                Log.d(TAG, "AdminGraph: onDrawerClick: ${drawerState.isClosed}")
+                                if (drawerState.isClosed) drawerState.open() else drawerState.close()
+                            }
+                        }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.List,
+                                contentDescription = "Back button"
+                            )
+                        }
+                    },
+                    scrollBehavior = scrollBehavior
+                )
+            }
+        ) { innerPadding ->
+            NavHost(
+                navController = navController,
+                startDestination = startDestination.route
+            ) {
+                Screen.Staff.getAllScreens().forEach { screen ->
+                    composable(screen.route) {
+                        when (screen.route) {
+                            Screen.Staff.ChatScreen.route -> {
+                                // ChatScreen()
+                            }
 
-                    Screen.Staff.TeamAnalytics.route -> {
-                        // TeamAnalytics()
+                            Screen.Staff.OrderManagement.route -> {
+                                // Reports()
+                            }
+                        }
                     }
                 }
             }
