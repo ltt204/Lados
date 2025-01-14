9e03b4611ced42bb49f4f5c9aa70c3ca4f7023fd
Ly'TTin <22120371@student.hcmus.edu.vn>
2025-01-14 08:06:10 +0700

    Fixing UI of CartItemBar; extract some fixed string

--- app/src/main/java/org/nullgroup/lados/compose/cart/CartItemBar.kt
+++ app/src/main/java/org/nullgroup/lados/compose/cart/CartItemBar.kt
@@ -1,13 +1,18 @@
 package org.nullgroup.lados.compose.cart
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FilledIconButton
@@ -26,16 +31,18 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
 import org.nullgroup.lados.R
-import org.nullgroup.lados.compose.common.TwoColsItem
 import org.nullgroup.lados.screens.customer.cart.ItemState
 import org.nullgroup.lados.ui.theme.LadosTheme
 
 @Composable
 fun CartItemBar(
+    modifier: Modifier = Modifier,
     imageUrl: String,
     title: String,
     originalPrice: String,
@@ -48,7 +55,6 @@ fun CartItemBar(
     quantity: Int = 0,
     onRemoveClick: (() -> Unit)? = null,
     itemState: ItemState? = ItemState.UNSELECTED,
-    modifier: Modifier = Modifier
 ) {
     if (quantity == 0) {
         return
@@ -85,216 +91,181 @@ fun CartItemBar(
 
     // Product Details
 
-    TwoColsItem(
+    Card(
+        modifier = Modifier
+            .heightIn(max = 128.dp)
+            .fillMaxWidth()
+            .clip(LadosTheme.shape.medium),
+        onClick = {
+            onItemSelected()
+        },
         colors = CardDefaults.cardColors(
-            containerColor = containerColor,
-            contentColor = textColor
-        ),
-        content = {
-            Row(
-                modifier = Modifier.fillMaxHeight(),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                SubcomposeAsyncImage(
-                    modifier = Modifier
-                        .size(100.dp)
-                        .clip(LadosTheme.shape.medium),
-                    model = ImageRequest
-                        .Builder(LocalContext.current)
-                        .crossfade(true)
-                        .data(imageUrl)
-                        .build(),
-                    contentDescription = "Product Image",
-                    contentScale = ContentScale.Crop,
-                    loading = {
-                        CircularProgressIndicator(modifier = Modifier.size(LadosTheme.size.medium))
-                    },
-                    error = {
-                        Text(text = "Image failed to load: ${it.result.throwable.message}")
-                    }
+            containerColor = LadosTheme.colorScheme.surfaceContainerHigh,
+            contentColor = LadosTheme.colorScheme.onPrimaryContainer,
+            disabledContainerColor = LadosTheme.colorScheme.outline,
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .wrapContentHeight()
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            SubcomposeAsyncImage(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .size(100.dp)
+                    .clip(LadosTheme.shape.medium),
+                model = ImageRequest
+                    .Builder(LocalContext.current)
+                    .crossfade(true)
+                    .data(imageUrl)
+                    .build(),
+                contentDescription = "Product Image",
+                contentScale = ContentScale.Crop,
+                loading = {
+                    CircularProgressIndicator(modifier = Modifier.size(LadosTheme.size.medium))
+                },
+                error = {
+                    Text(text = "Image failed to load: ${it.result.throwable.message}")
+                }
+            )
+            Spacer(modifier = Modifier.padding(horizontal = LadosTheme.size.small))
+            Column {
+                Text(
+                    text = title,
+                    style = LadosTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
                 )
-                Spacer(modifier = Modifier.padding(horizontal = LadosTheme.size.small))
-                Column(
-                    modifier = Modifier.fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceBetween
-                ) {
-                    Text(
-                        text = title,
-                        style = LadosTheme.typography.bodyLarge.copy(fontWeight = FontWeight.SemiBold),
-                    )
-                    Column {
-                        Text(
-                            text = "Size: $size",
-                            style = LadosTheme.typography.bodySmall
-                        )
-                        Text(
-                            text = "Color: $color",
-                            style = LadosTheme.typography.bodySmall
-                        )
-                    }
-
+                Spacer(modifier = Modifier.height(LadosTheme.size.small))
+                Row {
                     Column(
-                        modifier = Modifier,
+                        modifier = Modifier
+                            .fillMaxHeight()
+                            .weight(1f),
+                        verticalArrangement = Arrangement.SpaceBetween
                     ) {
-                        val isSale = salePrice != null
-                        Log.d("CartItemBar", "originalPrice: $originalPrice, salePrice: $salePrice, isSale: $isSale")
-                        Text(
-                            text = originalPrice,
-                            textDecoration = if (isSale) TextDecoration.LineThrough else null,
-                            style = LadosTheme.typography.titleMedium.copy(fontWeight = FontWeight.SemiBold),
-                        )
-                        if (salePrice != null) {
+                        Column {
                             Text(
-                                text = salePrice,
+                                text = "Size: $size",
+                                style = LadosTheme.typography.bodySmall
+                            )
+                            Text(
+                                text = "Color: $color",
+                                style = LadosTheme.typography.bodySmall
+                            )
+                        }
+                        Column(
+                            modifier = Modifier,
+                        ) {
+                            val isSale = salePrice != null
+                            if (salePrice != null) {
+                                Text(
+                                    text = salePrice,
+                                    style = LadosTheme.typography.titleMedium.copy(
+                                        fontWeight = FontWeight.SemiBold,
+                                        color = LadosTheme.colorScheme.error,
+                                        textAlign = TextAlign.Center
+                                    ),
+                                )
+                            }
+                            Spacer(modifier = Modifier.width(4.dp))
+                            Text(
+                                text = originalPrice,
+                                textDecoration = if (isSale) TextDecoration.LineThrough else null,
                                 style = LadosTheme.typography.titleMedium.copy(
                                     fontWeight = FontWeight.SemiBold,
-                                    color = LadosTheme.colorScheme.error,
-                                    textAlign = TextAlign.Center
+                                    fontSize = if (isSale) 14.sp else 16.sp,
                                 ),
                             )
                         }
+
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxHeight(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        if (onRemoveClick != null) {
+                            FilledIconButton(
+                                onClick = onRemoveClick,
+                                enabled = clickEnabled,
+                                colors = iconButtonColors,
+                                modifier = Modifier
+                                    .scale(0.75f)
+                                    .size(36.dp)
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.icon_remove),
+                                    contentDescription = "Add",
+                                )
+                            }
+                        }
+                        if (onAddClick == null) {
+                            Text(
+                                text = stringResource(id = R.string.order_items_header, quantity),
+                                style = bodyLargeTypo
+                            )
+                        } else {
+                            Text(
+                                text = quantity.toString(),
+                                style = bodyLargeTypo
+                            )
+                        }
+                        if (onAddClick != null) {
+                            // Spacer(modifier = Modifier.width(4.dp))
+                            FilledIconButton(
+                                onClick = onAddClick,
+                                enabled = clickEnabled,
+                                colors = iconButtonColors,
+                                modifier = Modifier
+                                    .scale(0.75f)
+                                    .size(36.dp)
+                            ) {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.icon_add),
+                                    contentDescription = "Remove",
+                                )
+                            }
+                        }
+
                     }
                 }
             }
-        },
-        trailingAction = {
-            Row(
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                if (onRemoveClick != null) {
-                    FilledIconButton(
-                        onClick = onRemoveClick,
-                        enabled = clickEnabled,
-                        colors = iconButtonColors,
-                        modifier = Modifier.scale(0.75f)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.icon_remove),
-                            contentDescription = "Add",
-                        )
-                    }
-                }
-                if (onAddClick == null) {
-                    Text(
-                        text = stringResource(id = R.string.order_items_header, quantity),
-                        style = bodyLargeTypo
-                    )
-                } else {
-
-                    Text(
-                        text = quantity.toString(),
-                        style = bodyLargeTypo
-                    )
-                }
-                if (onAddClick != null) {
-                    // Spacer(modifier = Modifier.width(4.dp))
-                    FilledIconButton(
-                        onClick = onAddClick,
-                        enabled = clickEnabled,
-                        colors = iconButtonColors,
-                        modifier = Modifier.scale(0.75f)
-                    ) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.icon_add),
-                            contentDescription = "Remove",
-                        )
-                    }
-                }
-
-            }
-        },
-        onClick = {
-            onItemSelected()
         }
-    )
+    }
 }
-//        Column(
-//            modifier = Modifier
-//                .weight(1f)
-//                .padding(end = 4.dp)
-//        ) {
-//            Text(
-//                text = title,
-//                style = bodyMediumTypo.copy(fontWeight = FontWeight.Bold),
-//                maxLines = 2,
-//                overflow = TextOverflow.Ellipsis
-//            )
-//            Spacer(modifier = Modifier.height(8.dp))
-//
-//            val boldSpanStyle = bodySmallTypo.toSpanStyle().copy(
-//                fontWeight = FontWeight.Bold
-//            )
-//            val normalSpanStyle = bodySmallTypo.toSpanStyle()
-//            Text(
-//                text = buildAnnotatedString {
-//                    withStyle(normalSpanStyle) {
-//                        append(stringResource(R.string.cart_item_size))
-//                    }
-//                    withStyle(boldSpanStyle) {
-//                        append(size)
-//                    }
-//                },
-//                maxLines = 1,
-//                overflow = TextOverflow.Ellipsis
-//            )
-//            Text(
-//                text = buildAnnotatedString {
-//                    withStyle(normalSpanStyle) {
-//                        append(stringResource(R.string.cart_item_color))
-//                    }
-//                    withStyle(boldSpanStyle) {
-//                        append(color)
-//                    }
-//                },
-//                maxLines = 1,
-//                overflow = TextOverflow.Ellipsis
-//            )
-//            Column(
-//                verticalArrangement = Arrangement.spacedBy(4.dp),
-////                horizontalArrangement = Arrangement.spacedBy(4.dp),
-////                modifier = Modifier.align(Alignment.End)
-//            ) {
-//                if (originalPrice != salePrice) {
-//                    Text(
-//                        text = originalPrice,
-//                        style = bodyMediumTypo.copy(
-//                            textDecoration = TextDecoration.LineThrough,
-//                            fontWeight = FontWeight.Bold,
-//                        )
-//                    )
-//
-//                }
-//                Text(
-//                    text = salePrice,
-//                    style = bodyMediumTypo.copy(fontWeight = FontWeight.Bold),
-//                )
-//            }
-//        }
 
-// Price and Actions
-//    Column(
-//        horizontalAlignment = Alignment.End,
-//        modifier = Modifier.wrapContentWidth(),
-//    ) {
-//
-////            Row {
-////                Text(
-////                    text = buildAnnotatedString {
-////                        append("x")
-////                        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
-////                            append(quantity.toString())
-////                        }
-////                    },
-////                    style = bodyLargeTypo,
-////                )
-////            }
-//
-////            val iconButtonColors = IconButtonColors(
-////                contentColor = Color.White,
-////                containerColor = Color(0xFF9371FF),
-////                disabledContentColor = Color.Gray,
-////                disabledContainerColor = Color(0xFF9371FF).copy(alpha = 0.25f)
-////            )
+@Preview
+@Composable
+fun CartItemPreview() {
+    LadosTheme {
+        CartItemBar(
+            imageUrl = "https://via.placeholder.com/150",
+            title = "Long Product Title For Testing",
+            originalPrice = "$100",
+            salePrice = "$80",
+            size = "M",
+            color = "Red",
+            quantity = 1,
+            onAddClick = {},
+            onRemoveClick = {}
+        )
+    }
+}
 
-
+@Preview
+@Composable
+fun CheckoutItemPreview() {
+    LadosTheme {
+        CartItemBar(
+            imageUrl = "https://via.placeholder.com/150",
+            title = "Long Product Title For Testing",
+            originalPrice = "$100",
+            salePrice = "$80",
+            size = "M",
+            color = "Red",
+            quantity = 12,
+        )
+    }
+}
